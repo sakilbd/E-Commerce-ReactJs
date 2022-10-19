7699edd8864e59e420457b465dd5ab5c12c41a60
sakilbd <mdsakiluzzaman@gmail.com>
2022-10-20 00:46:59 +0600

    some canges

--- faker/test.js
+++ faker/test.js
@@ -1,18 +1,36 @@
-// const faker = require('faker');
-const generatePersonsData = (number) => {
-    const persons = [];
-    while (number >= 0) {
-        persons.push({
-            id: Math.random(),
-            // name: faker.name.firstName(),
-            // description: faker.lorem.paragraphs(2),
-            // picture: faker.image.avatar(),
-            // country: faker.address.country(),
-            // joining_date: faker.date.future(),
+var casual = require('casual');
+var fs = require('fs');
+
+var title = casual._title();
+// Same as
+var title = casual.title;
+
+// Pass generator as callback
+var array_of = function(times) {
+    var result = [];
+
+    for (var i = 0; i < times; ++i) {
+        casual.define('user', function() {
+            return {
+                email: casual.email,
+                firstname: casual.first_name,
+                lastname: casual.last_name,
+                password: casual.password
+            };
         });
-        number--;
+
+        // Generate object with randomly generated fields
+        var user = casual.user;
+        result.push(user);
     }
-    return persons;
+
+    return result;
 };
 
-console.log(generatePersonsData(10));
+// Will generate array of five random timestamps
+var data = array_of(5);
+console.log(data)
+fs.writeFile("./faker/db.json", JSON.stringify(data), function(err) {
+    if (err) throw err;
+    console.log('complete');
+});
